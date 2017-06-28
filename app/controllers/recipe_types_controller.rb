c5a7845f41654412ce98b864a29351dfdb6fe3e6
JoaoHenriqueVale <jhm.vale@gmail.com>
2017-06-27 21:50:51 -0300

    Administrador cadastra tipos de receita

--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -4,7 +4,25 @@ class RecipeTypesController < ApplicationController
   def show
   end
 
+  def new
+    @recipe_type = RecipeType.new
+  end
+
+  def create
+    @recipe_type = RecipeType.new(recipe_type_params)
+    if @recipe_type.save
+      redirect_to recipe_type_url(@recipe_type.id)
+    else
+      flash[:error] = "Você deve informar o nome do tipo de receita"
+      render :new
+    end
+  end
+
   private
+
+  def recipe_type_params
+      params.require(:recipe_type).permit(:name)
+  end
   def set_recipe_type
     @recipe_type = RecipeType.find(params[:id])
   end
